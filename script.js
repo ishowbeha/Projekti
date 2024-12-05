28c8eac8741188e307147161fee9d5f709b1e230
loramulaku <l0ramulaku5@gmail.com>
2024-12-05 13:21:37 +0100

    rregullimi i problemit me slider

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-var i = 0;
-var imgArray = [
-    "foto1.avif",
-    "foto2.jpg",
-    "foto3.jpg",
-    "foto4.jpg",
-    "foto6.jpg"
-];
-/*
-function ndrroImg() {
-    document.getElementById('slideshow').src = imgArray[i];
-    if (i < imgArray.length - 1) {
-        i++;
-    } else {
-        i = 0;
+
+    // Inicializo variablin i për indeksin dhe array-n e imazheve
+    var i = 0;
+    var imgArray = [
+        "foto1.avif",
+        "foto2.jpg",
+        "foto3.jpg",
+        "foto4.jpg",
+        "foto6.avif"
+    ];
+
+    function ndrroImg() {
+        // Përditëso burimin e imazhit me indeksin aktual
+        document.getElementById('slideshow').src = imgArray[i];
+
+        // Nëse është imazhi i fundit, kthehu te i pari
+        if (i < imgArray.length - 1) {
+            i ++;; // Kthehu te fillimi
+        } else {
+            i=0; // Kalon te imazhi tjetër
+        }
     }
-}
-*/
+    document.body.addEventListener('load', ndrroImg());
+    // Sigurohu që slider-i të ngarkohet te imazhi i parë në fillim
+   
 
-document.body.addEventListener('load', ndrroImg());
-
-
-function scrollToSlider() {
-    document.getElementById('kontenti').scrollIntoView({ behavior: 'smooth' });
-}
-
-
-function ndrroImg() {
-    document.getElementById('slideshow').src = imgArray[i];
-    i = (i + 1) % imgArray.length; // Përditëso indeksin në mënyrë ciklike
-}
