472934cd92b8e2e755453634a7faa485d8286aea
ishowbeha <beharsurdulli6@gmail.com>
2024-12-08 02:48:49 +0100

    Validimi i sign up dhe implementimi i nav bar tek pages tjera

--- LogIn1.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(ngjarja) {
-    const SubmitButon = document.getElementById('btn');
-    
-    const validate = (ngjarja) => {
-        ngjarja.preventDefault();
-    }
-});
-
-
---
+++ sign_up.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function(ngjarja) {
+    const SubmitButon = document.getElementById('btn');
+    
+    const validate = (ngjarja) => {
+        ngjarja.preventDefault();
+        const emri = document.getElementById('emri');
+        const mbiemri = document.getElementById('mbiemri');
+        const password = document.getElementById('pass');
+        const confirmPass = document.getElementById('cpass');
+        const email = document.getElementById('email');
+        if(emri.value === ""){
+            alert("Ju lutem shtoni EMRIN tuaj");
+            emri.focus();
+            return false;
+        }
+        if(mbiemri.value === ""){
+            alert("Ju lutem shtoni MBIEMRIN tuaj");
+            mbiemri.focus();
+            return false;
+        }
+        if(password.value === ""){
+            alert("Ju lutem shtoni PASSWORDIN tuaj");
+            password.focus();
+            return false;
+        }
+        if(confirmPass.value === ""){
+            alert("Ju lutem KONFIRMONI PASSWORDIN tuaj");
+            confirmPass.focus();
+            return false;
+        }
+        if(email.value === ""){
+            alert("Ju lutem shtoni EMAIL tuaj");
+            email.focus();
+            return false;
+        }
+        if(!emailValid(email.value)){
+            alert("Ju lutem shtoni Email valid");
+            email.focus();
+            return false;
+        }
+        alert("Regjistrimi u krue me sukses!");
+        return true;
+    }
+    
+
+    const emailValid = (email)=> {
+        const emailRegex = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+        return emailRegex.test(email.toLowerCase());
+    }
+    SubmitButon.addEventListener('click', validate);
+    
+    
+});
+
+
